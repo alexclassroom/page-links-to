3aa2c5d67f87e5c1df08086b22772e5fc75b4ba8
Mark Jaquith <mark@jaquith.me>
2026-02-14 02:29:40 -0500

    Fix deprecated block editor warnings (fixes #187)
    Replace wp.compose.withState with useState hook and convert LinksTo
    from a class component to a functional component using React hooks.
    Replace wp.editPost.PluginDocumentSettingPanel import with
    wp.editor.PluginDocumentSettingPanel. Update PHP enqueue to read
    dependencies from the webpack-generated asset file.

--- src/block-editor.js
+++ src/block-editor.js
@@ -5,13 +5,10 @@ import {
 	CheckboxControl,
 	createSlotFill,
 } from '@wordpress/components';
-import { withInstanceId, compose, withState } from '@wordpress/compose';
-import { withSelect, withDispatch } from '@wordpress/data';
-import { Fragment, Component } from '@wordpress/element';
-import {
-	PluginDocumentSettingPanel,
-	PluginPostStatusInfo,
-} from '@wordpress/edit-post';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useState, useRef } from '@wordpress/element';
+import { PluginDocumentSettingPanel } from '@wordpress/editor';
+import { PluginPostStatusInfo } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
 
 const panelTitle = window?.pltOptions?.panelTitle ?? 'Page Links To';
@@ -37,157 +34,112 @@ const Placement = PluginDocumentSettingPanel || BackCompatPlacement;
 
 const { Slot } = createSlotFill('PageLinksToSidebar');
 
-class LinksTo extends Component {
-	constructor(props) {
-		super(props);
-		this.toggleStatus = this.toggleStatus.bind(this);
-		this.state.enabled = this.hasUrl();
-	}
+const PointsTo = ({ enabled, onToggle }) => {
+	const [option, setOption] = useState(enabled ? 'custom' : 'wordpress');
 
-	state = {
-		prevUrl: '',
-		prevNewTab: false,
-	};
+	return (
+		<RadioControl
+			label="Point this content to:"
+			selected={option}
+			options={[
+				{ label: 'Its normal WordPress URL', value: 'wordpress' },
+				{ label: 'A custom URL', value: 'custom' },
+			]}
+			onChange={(newOption) => {
+				setOption(newOption);
+				onToggle(newOption === 'custom');
+			}}
+		/>
+	);
+};
 
-	getUrl() {
-		return this.props.url || '';
-	}
-
-	getDisplayUrl() {
-		const { prevUrl } = this.state;
-		return this.getUrl() || prevUrl;
-	}
-
-	hasUrl() {
-		return this.getUrl().length > 0;
-	}
-
-	opensInNewTab() {
-		return this.props.newTab;
-	}
-
-	enabled() {
-		return this.state.enabled;
-	}
-
-	toggleStatus(newValue) {
-		const { prevUrl, prevNewTab } = this.state;
-		const { onUpdateLink, onUpdateNewTab } = this.props;
-
-		this.setState((prevState) => {
-			const newState = {
-				enabled: newValue,
-			};
-
-			if (prevState.enabled) {
-				newState.prevUrl = this.getUrl();
-			}
-
-			return newState;
-		});
-
-		if (newValue) {
-			// We should restore the previous states of the url and new tab checkbox.
-			onUpdateLink(prevUrl);
-			onUpdateNewTab(prevNewTab);
-		} else {
-			onUpdateLink(null);
-			onUpdateNewTab(false);
-
-			// Hold on to the previous state, in case they change their mind.
-			this.setState({
-				prevUrl: this.getUrl(),
-				prevNewTab: this.opensInNewTab(),
-			});
-		}
-	}
-
-	updateLink(link) {
-		const { meta, onUpdateLink } = this.props;
-		onUpdateLink(meta, link);
-	}
-
-	render() {
-		const { onUpdateLink, onUpdateNewTab } = this.props;
-		const PointsTo = withState({
-			option: this.enabled() ? 'custom' : 'wordpress',
-		})(({ option, setState }) => (
-			<RadioControl
-				label="Point this content to:"
-				selected={option}
-				options={[
-					{ label: 'Its normal WordPress URL', value: 'wordpress' },
-					{ label: 'A custom URL', value: 'custom' },
-				]}
-				onChange={(option) => {
-					setState({ option });
-					this.toggleStatus(option === 'custom');
-				}}
-			/>
-		));
-
-		return (
-			<Placement
-				title={panelTitle}
-				name="PageLinksTo"
-				icon={this.enabled() ? 'admin-links' : 'disabled'}
-				className="plt-panel"
-			>
-				<PanelRow>
-					<PointsTo />
-				</PanelRow>
-
-				{this.enabled() && (
-					<>
-						<PanelRow>
-							<TextControl
-								label="Links to"
-								data-testid="plt-url"
-								value={this.getDisplayUrl()}
-								onChange={onUpdateLink}
-								placeholder="https://"
-							/>
-						</PanelRow>
-						{window.pltOptions.supports.newTab && (
-							<PanelRow>
-								<CheckboxControl
-									label="Open in new tab"
-									data-testid="plt-newtab"
-									checked={this.opensInNewTab()}
-									onChange={onUpdateNewTab}
-								/>
-							</PanelRow>
-						)}
-						<Slot />
-					</>
-				)}
-			</Placement>
-		);
-	}
-}
-
-const PageLinksTo = compose([
-	withSelect((select) => {
+const LinksTo = () => {
+	const { url, newTab } = useSelect((select) => {
 		const getMeta = (attr) =>
 			(select('core/editor').getEditedPostAttribute('meta') || [])[attr];
 		return {
 			url: getMeta('_links_to'),
 			newTab: getMeta('_links_to_target') === '_blank',
 		};
-	}),
-	withDispatch((dispatch) => ({
-		onUpdateLink: (link) => {
-			dispatch('core/editor').editPost({ meta: { _links_to: link } });
-		},
-		onUpdateNewTab: (enabled) => {
-			dispatch('core/editor').editPost({
-				meta: { _links_to_target: enabled ? '_blank' : '' },
-			});
-		},
-	})),
-	withInstanceId,
-])(LinksTo);
+	}, []);
+
+	const { editPost } = useDispatch('core/editor');
+
+	const onUpdateLink = (link) => {
+		editPost({ meta: { _links_to: link } });
+	};
+
+	const onUpdateNewTab = (enabled) => {
+		editPost({ meta: { _links_to_target: enabled ? '_blank' : '' } });
+	};
+
+	const currentUrl = url || '';
+	const hasUrl = currentUrl.length > 0;
+
+	const [enabled, setEnabled] = useState(hasUrl);
+	const prevUrlRef = useRef('');
+	const prevNewTabRef = useRef(false);
+
+	const displayUrl = currentUrl || prevUrlRef.current;
+
+	const toggleStatus = (newValue) => {
+		if (!newValue && enabled) {
+			// Hold on to the previous state, in case they change their mind.
+			prevUrlRef.current = currentUrl;
+			prevNewTabRef.current = newTab;
+		}
+
+		setEnabled(newValue);
+
+		if (newValue) {
+			// Restore the previous states of the url and new tab checkbox.
+			onUpdateLink(prevUrlRef.current);
+			onUpdateNewTab(prevNewTabRef.current);
+		} else {
+			onUpdateLink(null);
+			onUpdateNewTab(false);
+		}
+	};
+
+	return (
+		<Placement
+			title={panelTitle}
+			name="PageLinksTo"
+			icon={enabled ? 'admin-links' : 'disabled'}
+			className="plt-panel"
+		>
+			<PanelRow>
+				<PointsTo enabled={enabled} onToggle={toggleStatus} />
+			</PanelRow>
+
+			{enabled && (
+				<>
+					<PanelRow>
+						<TextControl
+							label="Links to"
+							data-testid="plt-url"
+							value={displayUrl}
+							onChange={onUpdateLink}
+							placeholder="https://"
+						/>
+					</PanelRow>
+					{window.pltOptions.supports.newTab && (
+						<PanelRow>
+							<CheckboxControl
+								label="Open in new tab"
+								data-testid="plt-newtab"
+								checked={newTab}
+								onChange={onUpdateNewTab}
+							/>
+						</PanelRow>
+					)}
+					<Slot />
+				</>
+			)}
+		</Placement>
+	);
+};
 
 registerPlugin('page-links-to', {
-	render: PageLinksTo,
+	render: LinksTo,
 });
